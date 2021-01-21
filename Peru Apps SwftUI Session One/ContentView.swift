90182281877fc1020dbbb13ad232171738f89bad
Nicolás A. Rodríguez <nrq5510@gmail.com>
2021-01-21 17:12:17 -0500

    Initial Commit

---
+++ Peru Apps SwftUI Session One/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Peru Apps SwftUI Session One
+//
+//  Created by Nicolás A. Rodríguez on 21/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peru Apps SwftUI Session One/Peru_Apps_SwftUI_Session_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peru_Apps_SwftUI_Session_OneApp.swift
+//  Peru Apps SwftUI Session One
+//
+//  Created by Nicolás A. Rodríguez on 21/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Peru_Apps_SwftUI_Session_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
